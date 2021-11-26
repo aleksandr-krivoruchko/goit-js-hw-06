4bec55f74cd507529e61c1bdffad397adb95d307
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2021-11-26 13:48:21 +0200

    Done task 2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,39 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// Potatoes
+const firstIngrEl = document.createElement('li');
+firstIngrEl.textContent = 'Potatoes';
+firstIngrEl.classList.add = 'item';
+
+//Mushrooms
+const secondIngrEl = document.createElement('li');
+secondIngrEl.textContent = 'Mushrooms';
+secondIngrEl.classList.add = 'item';
+
+//Garlic
+const thirdIngrEl = document.createElement('li');
+thirdIngrEl.textContent = 'Garlic';
+thirdIngrEl.classList.add = 'item';
+
+//Tomatos
+const fourIngrEl = document.createElement('li');
+fourIngrEl.textContent = 'Tomatos';
+fourIngrEl.classList.add = 'item';
+
+//Herbs
+const fifthIngrEl = document.createElement('li');
+fifthIngrEl.textContent = 'Herbs';
+fifthIngrEl.classList.add = 'item';
+
+//Condiments
+const sixthIngrEl = document.createElement('li');
+sixthIngrEl.textContent = 'Condiments';
+sixthIngrEl.classList.add = 'item';
+
+
+
+
+
+
