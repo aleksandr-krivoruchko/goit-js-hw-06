7563f44248c02c5ee8e4db4f31ae9c4cb70fdb04
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2021-12-01 21:11:36 +0200

    Task 8 not finished

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
 
+const formEl = document.querySelector('.login-form');
+const btnEl = formEl.lastElementChild;
+
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+	event.preventDefault();
+
+	const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return window.alert("Please fill in all the fields!");
+
+  }
+console.log(`elements);
+
+  event.currentTarget.reset();
+}
