dcd390986018c357c3d22db01c12799abd0c95df
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2021-12-04 21:22:10 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,41 +6,16 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-
-// Potatoes
-const firstIngrEl = document.createElement('li');
-firstIngrEl.textContent = 'Potatoes';
-firstIngrEl.classList.add = 'item';
-
-//Mushrooms
-const secondIngrEl = document.createElement('li');
-secondIngrEl.textContent = 'Mushrooms';
-secondIngrEl.classList.add = 'item';
-
-//Garlic
-const thirdIngrEl = document.createElement('li');
-thirdIngrEl.textContent = 'Garlic';
-thirdIngrEl.classList.add = 'item';
-
-//Tomatos
-const fourthIngrEl = document.createElement('li');
-fourthIngrEl.textContent = 'Tomatos';
-fourthIngrEl.classList.add = 'item';
-
-//Herbs
-const fifthIngrEl = document.createElement('li');
-fifthIngrEl.textContent = 'Herbs';
-fifthIngrEl.classList.add = 'item';
-
-//Condiments
-const sixthIngrEl = document.createElement('li');
-sixthIngrEl.textContent = 'Condiments';
-sixthIngrEl.classList.add = 'item';
-
 const listEl = document.querySelector('#ingredients');
-listEl.append(firstIngrEl, secondIngrEl, thirdIngrEl, 
-	           fourthIngrEl, fifthIngrEl, sixthIngrEl);
 
+let arrayOfIngredients = [];
 
+for (let i = 0; i < ingredients.length; i++) {
+	
+	const ingrEl = document.createElement('li');
+	ingrEl.textContent = ingredients[i];
+	ingrEl.classList.add = 'item';
+	arrayOfIngredients.push(ingrEl);
+}
 
-
+listEl.append(...arrayOfIngredients);
