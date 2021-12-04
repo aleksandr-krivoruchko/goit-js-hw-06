943573a0c80a6a5da49c0b3e15846df7fa1ea7c7
aleksandr-krivoruchko <krivouzis1985@gmail.com>
2021-12-04 20:56:39 +0200

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -2,34 +2,7 @@
 const itemsEl = document.querySelectorAll('.item');
 console.log(`Number of categories: ${itemsEl.length}`);
 
-
-// Animals
-
-const firstItemEl = document.querySelector('.item');
-const listOfFirstItemEl = firstItemEl.querySelectorAll('li');
-const titleOfFirstEl = firstItemEl.querySelector('h2');
-
-console.log(`Category: ${titleOfFirstEl.textContent}`);
-console.log(`Elements: ${listOfFirstItemEl.length}`);
-
-
-// Products
-
-const secondItemEl = firstItemEl.nextElementSibling;
-const listOfSecondItemEl = secondItemEl.querySelectorAll('li');
-const titleOfSecondeEl = secondItemEl.querySelector('h2');
-
-console.log(`Category: ${titleOfSecondeEl.textContent}`);
-console.log(`Elements: ${listOfSecondItemEl.length}`);
-
-
-// Technologies
-
-const thirdItemEl = secondItemEl.nextElementSibling;
-const listOfThirdItemEl = thirdItemEl.querySelectorAll('li');
-const titleOfThirdEl = thirdItemEl.querySelector('h2');
-
-console.log(`Category: ${titleOfThirdEl.textContent}`);
-console.log(`Elements: ${listOfThirdItemEl.length}`);
-
-
+for (let i = 0; i < itemsEl.length; i+=1) {
+	console.log(`Category: ${itemsEl[i].firstElementChild.textContent}`);
+   console.log(`Elements: ${itemsEl[i].lastElementChild.querySelectorAll('li').length}`);
+}
